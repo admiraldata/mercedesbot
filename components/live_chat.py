4bad9043ec6c1a240b0b42e3b220beb93fc6616a
mashakos <mohamed.alarabi@socialize.ae>
2016-08-16 11:49:23 +0400

    live chat

--- components/live_chat.py
+++ components/live_chat.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
+import requests
 from meya import Component
 
 
 class HelloWorld(Component):
 
     def start(self):
-        text = "Hello, world!"
+        url = 'http://slz.io/mb-bot/api/livechat'
+        data = {
+            'first_name': self.db.user.get('first_name'),
+            'last_name': self.db.user.get('last_name'),
+            'locale': self.db.user.get('locale'),
+            'timezone': self.db.user.get('timezone'),
+            'gender': self.db.user.get('gender')
+        }
+        headers = {
+        #'authorization': 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZGVudGl0eV9pZCI6MSwiaWRlbnRpdHlfdHlwZSI6IlVzZXIiLCJkYXRlX2NyZWF0ZWQiOiIyMDE2LTAxLTI1IDExOjQ3OjA1WiJ9.7xvbVBwpvZyVWOHdKZ0LDGG4aRpooT6ZN7m-X4YQLto',
+        'cache-control' : 'no-cache',
+        'content-type' : 'application/x-www-form-urlencoded',
+        }
+        response = requests.post(
+            url=url,
+            headers=headers,
+            json=data
+        )
+        response_json = response.json()
+        print response_json
+        if response_json.has_key('message'):
+            text=response_json['message']
         message = self.create_message(text=text)
-        return self.respond(message=message, action="next")
+        action = "next"
+        return self.respond(message=message, action=action)
